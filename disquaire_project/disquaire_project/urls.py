5a8ecc5fd1138ba7b7a9e6c90bcdc88cd8acd53d
The Crow <crow.manga@gmail.com>
2021-01-21 09:08:35 +0100

    correction erreur

--- disquaire_project/disquaire_project/urls.py
+++ disquaire_project/disquaire_project/urls.py
@@ -14,16 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls import include,url
+from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
 ]
 
-if setting.DEBUG:
+if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
-        url(r'^__debug__/',include(debug_toolbar.urls)),
-    ]+ urlpatterns
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
