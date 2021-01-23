a641886cbbee064cd60a0c52dbdc0b51124db08e
The Crow <crow.manga@gmail.com>
2021-01-23 21:53:39 +0100

    ajout vue

--- disquaire_project/store/models.py
+++ disquaire_project/store/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+ARTISTS = {
+    'KORN':{'name':'KORN'},
+    'NIRVANA':{'name':'NIRVANA'},
+    'BABY-METAL':{'name':'BABY METAL'},
+    'LINKIN-PARK':{'name':'LINKIN PARK'},
+}
+
+
+ALBUMS = [
+    {'name': 'Mirror','artists':[ARTISTS['KORN']]},
+    {'name': 'Nevermind','artists':[ARTISTS['NIRVANA']]},
+    {'name': 'Babymetal','artists':[ARTISTS['BABY-METAL']]},
+    {'name': 'Meteora','artists':[ARTISTS['LINKIN-PARK']]},
+]
